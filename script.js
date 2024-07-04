5eafd43efd8d4132201d9fdfc0f56d123ef515bb
JoacoPuertas <joacopuertas3@gmail.com>
2024-07-04 15:30:20 -0300

    a

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-function agendar() {
-    const button = document.querySelector(".red") || document.querySelector(".bordo");
+function agendar(event) {
+    const button = event.target;
+    
     if (button.classList.contains("red")) {
         button.classList.remove("red");
         button.classList.add("bordo");
-        button.textContent = "Agendado"
-    } else {
+        button.textContent = "Agendado";
+    } else if (button.classList.contains("bordo")) {
         button.classList.remove("bordo");
         button.classList.add("red");
+        button.textContent = "Agendar";
     }
 }
+
